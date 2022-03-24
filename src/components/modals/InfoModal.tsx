75205814f0dd01f05b72f37fea4cd385e0c9fcc8
Carson Kompon <carsokompo@gmail.com>
2022-03-24 13:36:06 -0400

    Added open source info

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -10,11 +10,18 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="How to play:" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Guess the sequence of Turnip Boy characters. While we won't tell you
-        them all, there are rules that defines the way that characters are laid
-        out. There are no duplicates, some characters are in certain positions,
-        some characters are paired with others, ect. You'll have to play more to
-        find all the hidden rules!
+        Guess the sequence of{' '}
+        <a
+          href="https://snoozykazoo.com/games/turnip-boy-commits-tax-evasion"
+          className="underline font-bold"
+          target="_blank"
+        >
+          Turnip Boy
+        </a>{' '}
+        characters. While we won't tell you them all, there are rules that
+        defines the way that characters are laid out. There are no duplicates,
+        some characters are in certain positions, some characters are paired
+        with others, ect. You'll have to play more to find all the hidden rules!
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
@@ -58,6 +65,33 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
         Annie is not in the solution in any spot.
+      </p>
+
+      <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
+        This is an{' '}
+        <a
+          href="https://github.com/CarsonKompon/Turnipleon"
+          className="underline font-bold"
+          target="_blank"
+        >
+          open source
+        </a>{' '}
+        Wordle-like game inspired by{' '}
+        <a
+          href="https://dungleon.com"
+          className="underline font-bold"
+          target="_blank"
+        >
+          Dungleon
+        </a>
+        . Made by{' '}
+        <a
+          href="https://twitter.com/CarsonKompon"
+          className="underline font-bold"
+          target="_blank"
+        >
+          @CarsonKompon
+        </a>{' '}
       </p>
     </BaseModal>
   )
